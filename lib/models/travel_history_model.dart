dabc9b7ffcf1dedd818520df69f0949e99db6921
Jenver <jenvervince.inojales@cit.edu>
2023-11-11 23:09:09 +0800

    Added new screens language and healthRecords screens and its models

---
+++ lib/models/travel_history_model.dart
@@ -0,0 +1,24 @@
+class HealthRecordsData{
+  String title;
+  String description;
+
+  HealthRecordsData({
+    required this.title,
+    required this.description,
+  });
+}
+
+final List<HealthRecordsData> healthRecordsData = [
+  HealthRecordsData(
+    title: "Food & Environment Allergies",
+    description: "Common food allergens include milk, eggs and peanuts. Environmental allergies occur when your immune system mistakenly triggers a defensive response to certain substances in your environment. This response or allergic reaction can cause various symptoms, including congestion, watery eyes, a runny nose and fatigue.",
+  ),
+  HealthRecordsData(
+    title: "Drug Allergies",
+    description: "Any description about Food & Environment Allergies",
+  ),
+  HealthRecordsData(
+    title: "Food2 & Environment Allergies",
+    description: "Any description about Food & Environment Allergies",
+  ),
+];
